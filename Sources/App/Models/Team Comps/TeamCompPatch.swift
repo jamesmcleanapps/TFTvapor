0edab29f5448288e727c239c6339f01bf7c346c7
James Mclean <jamesmcleanapps@gmail.com>
2020-02-14 11:28:09 +0000

    Fixed Patch notes and updated team comps to patch 10.3

--- Sources/App/Models/Team Comps/TeamCompPatch.swift
+++ Sources/App/Models/Team Comps/TeamCompPatch.swift
@@ -10,7 +10,8 @@ import Vapor
 
 enum TeamCompPatch: String, Comparable, Codable, ReflectionDecodable {
     
-    case nineOneFive = "9.15", nineOneFiveB = "9.15b", nineOneSix = "9.16", nineOneSixB = "9.16b", nineOneSeven = "9.17", nineOneNine = "9.19", nineTwoZero = "9.20", nineTwoOne = "9.21", nineTwoTwo = "9.22", nineTwoThree = "9.23", nineTwoFour = "9.24", nineTwoFive = "9.25", tenOneZero = "10.1", tenOneZeroB = "10.1b", tenOneOne = "10.11", tenOneOneB = "10.11b", tenOneTwo = "10.12", tenOneTwoB = "10.12b"
+    case nineOneFive = "9.15", nineOneFiveB = "9.15b", nineOneSix = "9.16", nineOneSixB = "9.16b", nineOneSeven = "9.17", nineOneNine = "9.19", nineTwoZero = "9.20", nineTwoOne = "9.21", nineTwoTwo = "9.22", nineTwoThree = "9.23", nineTwoFour = "9.24", nineTwoFive = "9.25", tenOneZero = "10.1", tenOneZeroB = "10.1b", tenOneOne = "10.11", tenOneOneB = "10.11b", tenOneTwo = "10.12", tenOneTwoB = "10.12b", tenTwo = "10.2", tenThree = "10.3",
+    tenFour = "10.4", tenFive = "10.5", tenSix = "10.6", tenSeven = "10.7"
     
     static func ==(lhs: TeamCompPatch, rhs: TeamCompPatch) -> Bool {
         return lhs.sortOrder == rhs.sortOrder
@@ -44,20 +45,33 @@ enum TeamCompPatch: String, Comparable, Codable, ReflectionDecodable {
             return 10
         case .nineTwoFour:
             return 11
-            case .nineTwoFive:
-                return 12
-            case .tenOneZero:
-                return 13
-            case .tenOneZeroB:
-                return 14
-            case .tenOneOne:
-                return 15
-                case .tenOneOneB:
-                return 16
-            case .tenOneTwo:
-                return 17
-            case .tenOneTwoB:
-                return 18
+        case .nineTwoFive:
+            return 12
+        case .tenOneZero:
+            return 13
+        case .tenOneZeroB:
+            return 14
+        case .tenOneOne:
+            return 15
+        case .tenOneOneB:
+            return 16
+        case .tenOneTwo:
+            return 17
+        case .tenOneTwoB:
+            return 18
+        case .tenTwo:
+            return 19
+        case .tenThree:
+            return 20
+        case .tenFour:
+            return 21
+        case .tenFive:
+            return 22
+        case .tenSix:
+            return 23
+        case .tenSeven:
+            return 24
+            
         }
     }
     
